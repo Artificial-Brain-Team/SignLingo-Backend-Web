b032f04ccdfb5fcea45d9798ae6eb6689f95f4a9
William Riega <williammar954@hotmail.com>
2023-06-04 15:50:56 -0500

    feat: Save, Update, Delete of Answer Infrastructure

--- SignLingo.Infrastructure/AnswerMySQLInfrastructure.cs
+++ SignLingo.Infrastructure/AnswerMySQLInfrastructure.cs
@@ -26,6 +26,29 @@ public class AnswerMySQLInfrastructure : IAnswerInfrastructure
         
         return answers;
     }
-    
+
+    public async Task<bool> SaveAsync(Answer answer)
+    {
+        await _signLingoDbContext.Answers.AddAsync(answer);
+        await _signLingoDbContext.SaveChangesAsync();
+        return true;
+    }
+
+    public async Task<bool> UpdateAsync(int id, Answer answer)
+    {
+        var answerFounded = await _signLingoDbContext.Answers.FindAsync(id);
+
+        answerFounded.Answer_text = answer.Answer_text;
+        answerFounded.IsCorrect = answer.IsCorrect;
+
+        await _signLingoDbContext.SaveChangesAsync();
+        return true;
+    }
+
+    public async Task<bool> DeleteAsync(int id)
+    {
+        _signLingoDbContext.Answers.Remove(await _signLingoDbContext.Answers.FindAsync(id));
+        return true;  
+    }
 
 }
--- SignLingo.Infrastructure/IAnswerInfrastructure.cs
+++ SignLingo.Infrastructure/IAnswerInfrastructure.cs
@@ -6,4 +6,7 @@ public interface IAnswerInfrastructure
 {
     Task<List<Answer>> GetAllAsync();
     Task<List<Answer>> GetByExerciseIdAsync(int exerciseId);
+    public Task<bool> SaveAsync(Answer user);
+    public Task<bool> UpdateAsync(int id, Answer asnwer);
+    public Task<bool> DeleteAsync(int id);
 }
