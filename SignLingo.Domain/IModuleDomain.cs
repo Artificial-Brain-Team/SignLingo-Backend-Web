0ca36d4d83fd9f75de1f390024f4447e0cf6397d
William Riega <williammar954@hotmail.com>
2023-06-04 20:53:12 -0500

    feat: added module domain

---
+++ SignLingo.Domain/IModuleDomain.cs
@@ -0,0 +1,10 @@
+﻿using SignLingo.Infrastructure.Models;
+
+namespace SignLingo.Domain;
+
+public interface IModuleDomain
+{
+    public Task<bool> SaveAsync(Module module);
+    public Task<bool> UpdateAsync(int id, Module module);
+    public Task<bool> DeleteAsync(int id);
+}
