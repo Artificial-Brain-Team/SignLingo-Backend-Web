514c79b1b38b3fe4025303ec9c1262ce35fab72b
Cristian Quito <cristianandresquito@gmail.com>
2023-06-09 20:09:31 -0500

    fix: origin

--- SignLingo.API/Program.cs
+++ SignLingo.API/Program.cs
@@ -32,7 +32,7 @@ builder.Services.AddScoped<IUserModuleDomain, UserModuleDomain>();
 //cors
 builder.Services.AddCors(p => 
     p.AddPolicy("AllowOrigin", corsPolicyBuilder => {
-    corsPolicyBuilder.WithOrigins("https://localhost:5173/")
+    corsPolicyBuilder.WithOrigins()
         .AllowAnyMethod().
         AllowAnyHeader();
 }));
